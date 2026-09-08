f770bc31110f343fc5b62f630c58862abca4d2b0
Alneox <78831369+alneox@users.noreply.github.com>
2026-09-08 15:12:38 +0600

    added stripe key update for agentic32 middleware stack

--- infra/terraform/variables.tf
+++ infra/terraform/variables.tf
@@ -240,7 +240,7 @@ variable "billing_api_stripe_secret_key_secret_version" {
 variable "billing_api_stripe_webhook_signing_secret_id" {
   description = "Secret Manager secret ID for the Stripe webhook signing secret."
   type        = string
-  default     = "stripe-webhook-signing-secret-v3"
+  default     = "stripe-webhook-signing-secret-agentic3"
 }
 
 variable "billing_api_stripe_webhook_signing_secret_version" {
